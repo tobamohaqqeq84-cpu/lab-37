3b88ed1ac060f3ffac348600887315566631600d
tobam <tobamohaqqeq84@gmail.com>
2025-12-07 21:53:16 +0000

    starter code added

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
-
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+int main() {
+char a = 'A';
+cout << a << endl;
+cout << (int) a << endl;
+int b = 66;
+cout << b << endl;
+cout << (char) b << endl;
+return 0;
+}
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
